16f20827905e6441c6a4d1682b5e0dca7e1da08a
Baptist BENOIST <baptist.benoist@videomenthe.fr>
2019-10-15 20:03:40 +0200

    Remove prettier-plugin-import-sort

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-function ArgDecorator(_target: Record<string, any>, _propertyKey: string | symbol, _parameterIndex: number): void {
-    console.log(_target, _propertyKey, _parameterIndex)
+function ArgDecorator(
+  _target: Record<string, any>,
+  _propertyKey: string | symbol,
+  _parameterIndex: number
+): void {
+  console.log(_target, _propertyKey, _parameterIndex);
 }
 
 class MyClass {
-    myFunction(@ArgDecorator myArg: string){
-        console.log(myArg);
-    }
+  myFunction(@ArgDecorator myArg: string) {
+    console.log(myArg);
+  }
 }
 
-const foo = new MyClass;
+const foo = new MyClass();
 foo.myFunction("hello");
