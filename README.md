f019beafc3fc0f2b8d68e6d8c30afd9083cbe869
Baptist BENOIST <baptist.benoist@videomenthe.fr>
2019-10-15 19:13:18 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,3 @@
+# prettier-plugin-import-sort-arg-decorator-bug
+
+Reproducible [prettier-plugin-import-sort](https://github.com/ggascoigne/prettier-plugin-import-sort/) bug with TypeScript argument decorators.
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function ArgDecorator(_target: Record<string, any>, _propertyKey: string | symbol, _parameterIndex: number): void {
+    console.log(_target, _propertyKey, _parameterIndex)
+}
+
+class MyClass {
+    myFunction(@ArgDecorator myArg: string){
+        console.log(myArg);
+    }
+}
+
+const foo = new MyClass;
+foo.myFunction("hello");
